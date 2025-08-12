a80d4e6f55421af1036ebbef83f48dfa33cdd5c7
Paula Fernandez Aranda <paulafa8@gmail.com>
2025-08-12 14:51:44 +0200

    feat: momentCrontoller

--- src/main/java/dev/paula/controller/MomentController.java
+++ src/main/java/dev/paula/controller/MomentController.java
@@ -1,5 +1,22 @@
 package dev.paula.controller;
 
+import dev.paula.dtos.MomentDTO;
+import dev.paula.mappers.CharacterMapper;
+import dev.paula.models.Moment;
+import dev.paula.repository.MomentRepository;
+import dev.paula.singletons.MomentRepositorySingleton;
+
+
 public class MomentController {
     
+    private MomentRepository repository;
+
+    public MomentController(){
+        this.repository = MomentRepositorySingleton.getInstance();
+    }
+
+    public void StoreMoment(MomentDTO momentDTO){
+        Moment momentToSave = MomentMapper.toEntity(momentDTO);
+        repository.StoreMoment(momentToSave);
+    }
 }
---
+++ src/main/java/dev/paula/singletons/MomentControllerSingleton.java
@@ -0,0 +1,14 @@
+package dev.paula.singletons;
+
+import dev.paula.controller.MomentController;
+
+public class MomentControllerSingleton {
+    
+    private static final MomentController INSTANCE = new MomentController();
+
+    private MomentControllerSingleton() {}
+    
+    public static MomentController getInstance() {
+        return INSTANCE;
+    }
+}
