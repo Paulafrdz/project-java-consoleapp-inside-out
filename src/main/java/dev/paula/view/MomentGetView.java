0ab3f63606da88822e39ce81de0da92626f19503
Paula Fernandez Aranda <paulafa8@gmail.com>
2025-08-13 15:06:21 +0200

    feat: return empty

--- src/main/java/dev/paula/view/MomentGetView.java
+++ src/main/java/dev/paula/view/MomentGetView.java
@@ -7,10 +7,17 @@ import dev.paula.dtos.MomentDTOResponse;
 public class MomentGetView extends View{
     
     public static void printAllMoments(List<MomentDTOResponse> moments){
+         if (moments.isEmpty()) {
+            System.out.println("No hay momentos guardados.");
+            HomeView.printMenu();
+            return;
+        }
+
         System.out.print("Lista de momentos vividos: ");
         for (MomentDTOResponse moment : moments){
             System.out.println("Ocurrio el: " + moment.date() + " Título: " + moment.title() + " Descripción: " + moment.description() + " Emoción: " + moment.emotion());
         }
+       
         HomeView.printMenu();
     }
 
