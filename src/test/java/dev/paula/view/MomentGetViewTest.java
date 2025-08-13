c700d7fe12579d4fffe7fb038cc03123055de3ca
Paula Fernandez Aranda <paulafa8@gmail.com>
2025-08-13 14:59:17 +0200

    test: momentGetWiev

---
+++ src/test/java/dev/paula/view/MomentGetViewTest.java
@@ -0,0 +1,56 @@
+
+package dev.paula.view;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import dev.paula.dtos.MomentDTOResponse;
+import dev.paula.models.Emotion;
+import java.util.Date;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class MomentGetViewTest {
+
+    private final PrintStream PrintStream = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp(){
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void testPrintAllMoments(){
+        String expected = "Lista de momentos vividos: ";
+        List<MomentDTOResponse> moments = new ArrayList<>();
+        moments.add(new MomentDTOResponse(
+        1,
+        "Playa",
+        "un dia paseando por la playa",
+        Emotion.ALEGRIA, // Usa el enum correspondiente
+        new Date()       // Usa la fecha que quieras
+        ));
+
+        MomentGetView.printAllMoments(moments);
+
+        assertThat(outputStreamCaptor.toString().trim(), containsString(expected));
+        assertThat(outputStreamCaptor.toString().trim(), containsString("Playa"));
+        assertThat(outputStreamCaptor.toString().trim(), containsString("ALEGRÍA"));
+
+    } 
+
+   @AfterEach
+    void tearDown() {
+        System.setOut(PrintStream);
+    }
+
+
+}
