d1bab0644ab38a06355a2bd8ed76cc3ede2dd3f7
Paula Fernandez Aranda <paulafa8@gmail.com>
2025-08-14 09:45:47 +0200

    test: momentGetViewTest

--- src/main/java/dev/paula/view/View.java
+++ src/main/java/dev/paula/view/View.java
@@ -3,5 +3,6 @@ package dev.paula.view;
 import java.util.Scanner;
 
 public class View {
+    public static boolean TEST_MODE = false;
     protected static Scanner SCANNER = new Scanner(System.in);
 }
